ffbecb015aac62cefab0f8635d0dc16f557fd86e
RikuKetomaki <riku.ketomaki@gmail.com>
2023-03-25 13:59:45 +0200

    Added Sidebar and class names

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,23 +1,27 @@
 import React from 'react';
 import Logo from '../Images/Northern Pc Store Logo.png'
+import Sidebar from './Sidebar';
+
 
 export default function Navbar() {
     return (
         <nav class="navbar sticky-top navbar-expand-lg">
             <div class="container-fluid">
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-                <a class="navbar-brand" href="#"><img src = {Logo}/></a>
-                <div class="collapse navbar-collapse" id="navbarTogglerDemo03">
                     <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                        <li class="nav-item">
+                        <li class="nav-item ">
+                            <Sidebar />
+                        </li>                       
+                        <li>
+                            <a class="navbar-brand" href="#"><img src = {Logo}/></a>
+                        </li>
+                        <li class="nav-item nav-link">
                             <a class="nav-link" aria-current="page" href="#">Northern PC Store</a>
                         </li>
-                        <li class="nav-item">
+
+                        <li class="nav-item nav-link">
                             <a class="nav-link" href="#">Link</a>
                         </li>
-                        <li class="nav-item">
+                        <li class="nav-item nav-link">
                             <a class="nav-link" href="#">Link 2</a>
                         </li>
                     </ul>
@@ -25,7 +29,6 @@ export default function Navbar() {
                         <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search"/> 
                             <button class="btn btn-outline-success" type="submit">Search</button>
                     </form>
-                </div>
             </div>
         </nav>
     );
