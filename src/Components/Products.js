5828cfc57948aed36321ae208e08678faf80d37e
JuusoPesonen <c2peju00@students.oamk.fi>
2023-03-29 16:06:28 +0300

    Created new page element products.js

---
+++ src/Components/Products.js
@@ -0,0 +1,11 @@
+import axios from 'axios';
+import React, { useState, useEffect } from 'react';
+import { Link, useParams } from 'react-router-dom';
+
+export default function Categories() {
+    return (
+        <div id="products">
+            Products
+        </div>
+    )
+}
