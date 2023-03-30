4591d37205a838a8e1bb7e0880763260e06f0474
JuusoPesonen <c2peju00@students.oamk.fi>
2023-03-30 12:12:23 +0300

    Changed function name in products.js

--- src/Components/Products.js
+++ src/Components/Products.js
@@ -3,7 +3,7 @@ import React, { useState, useEffect } from 'react';
 import { Link, useParams } from 'react-router-dom';
 import * as FaIcons from 'react-icons/fa';
 
-export default function Categories() {
+export default function Products() {
     return (
         <div id="products">
             Products
