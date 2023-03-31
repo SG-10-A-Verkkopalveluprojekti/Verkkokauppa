077ac77c98a5ce3d2410736c13ccec0fdacc868c
JuusoPesonen <c2peju00@students.oamk.fi>
2023-03-31 14:31:49 +0300

    Created categories query in navbar.js

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,10 +1,26 @@
-import React from 'react';
-import Logo from '../Images/Northern Pc Store Logo.png'
+import Logo from '../Images/Northern Pc Store Logo.png';
 import Sidebar from './Sidebar';
-import * as FaIcons from 'react-icons/fa'
+import * as FaIcons from 'react-icons/fa';
+import axios from 'axios';
+import React,{useState,useEffect} from 'react';
+import { Link } from 'react-router-dom';
 
 
-export default function Navbar() {
+export default function Navbar({url}) {
+    const [categories,setCategories] = useState([]);
+    
+    useEffect(() => {
+      axios.get(url + 'products/getcategories.php')
+        .then((response) => {
+            const json = response.data;
+            console.log(json);
+            setCategories(json);
+        }).catch (error => {
+            alert(error.response === undefined ? error: error.response.data.error);
+        })
+    }, [])
+    
+
     return (
         <nav className="navbar sticky-top navbar-expand-lg">
             <div className="container-fluid">
@@ -17,6 +33,20 @@ export default function Navbar() {
                         </li>
                         <li className="nav-item nav-link">
                             <a className="nav-link" aria-current="page" href="#">Northern PC Store</a>
+                        </li>
+                        <li className='nav-item dp-1 dropdown'>
+                            <a className='nav-link dropdown-toggle' href="#" id="dropdown01"
+                            data-bs-toggle="dropdown" aria-expanded="false">Products</a>
+                            <ul className='dropdown-menu' aria-labelledby='dropdown01'>
+                                {categories.map(category => (
+                                    <li key={category.category_num}>
+                                        <Link
+                                            className='dropdown-item'
+                                            to={'/products/' + category.category_num}>{category.name}
+                                        </Link>
+                                    </li>
+                                ))}
+                            </ul>
                         </li>
 
                         <li className="nav-item nav-link">
