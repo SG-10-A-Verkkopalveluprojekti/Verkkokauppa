d3631cea0b810799f7c71515032e8d8badec93cf
n2posa00 <n2posa00@students.oamk.fi>
2023-04-04 14:15:47 +0300

    Imported axios into showItem

--- src/Pages/showItem.js
+++ src/Pages/showItem.js
@@ -1,4 +1,5 @@
 import React,{useState,useEffect} from 'react';
+import axios from 'axios';
 import { useParams } from 'react-router-dom';
 import SpecificItemBuy from '../Components/SpecificItemBuy';
 import SpecificItemInfo from '../Components/SpecificItemInfo';
