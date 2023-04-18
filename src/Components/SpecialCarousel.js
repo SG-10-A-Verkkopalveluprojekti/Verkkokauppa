5d2913724a4b9aaa4c1fcf49109ff2c297a807b4
JuusoPesonen <c2peju00@students.oamk.fi>
2023-04-18 12:55:28 +0300

    Used banner image for img src

--- src/Components/SpecialCarousel.js
+++ src/Components/SpecialCarousel.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Logo from '../Images/Northern Pc Store Logo.png'
 import Banner from '../Images/Northern PC Store Banner.png'
 import Banner2 from '../Images/SaleBanner.png'
+import Banner3 from '../Images/Northern PC Store Banner 2.png'
 
 export default function SpecialCarousel() {
     return (
@@ -19,7 +20,7 @@ export default function SpecialCarousel() {
                     <img src={Banner2} class="d-block w-100" alt="..." />
                 </div>
                 <div className="carousel-item">
-                    <img src={Logo} class="d-block w-100" alt="..." />
+                    <img src={Banner3} class="d-block w-100" alt="..." />
                 </div>
             </div>
             <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
