5eab0e66e604f8c2fc747f0baf3ca83289c400b8
n2posa00 <n2posa00@students.oamk.fi>
2023-04-19 15:11:14 +0300

    products for product carousel working

--- src/Components/ProductCarousel1.js
+++ src/Components/ProductCarousel1.js
@@ -1,6 +1,13 @@
 // Product carousel code has been taken from https://www.npmjs.com/package/react-multi-carousel
 import Carousel from 'react-multi-carousel';
 import 'react-multi-carousel/lib/styles.css';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import { Link, useParams } from 'react-router-dom';
+import * as TbIcons from 'react-icons/tb';
+
+const URL = "http://localhost:8000/products/getitems.php";
+const imagesURL = 'http://localhost:8000/images/';
 
 const responsive = {
     superLargeDesktop: {
@@ -23,14 +30,96 @@ const responsive = {
 };
 
 export default function ProductCarousel1() {
+    const [item1, setItem1] = useState("")
+    const [item2, setItem2] = useState("")
+    const [item3, setItem3] = useState("")
+    const [item4, setItem4] = useState("")
+    const [item5, setItem5] = useState("")
+    const [item6, setItem6] = useState("")
+
+    useEffect(() => {
+        axios.get(URL)
+            .then((response) => {
+                const json = response.data
+                setItem1(json[5])
+                setItem2(json[2])
+                setItem3(json[6])
+                setItem4(json[9])
+                setItem5(json[12])
+                setItem6(json[15])
+            }).catch(error => {
+                alert(error.response === undefined ? error : error.response.data.error);
+            })
+    }, [])
+
     return (
         <Carousel responsive={responsive}>
-            <div>Item 1</div>
-            <div>Item 2</div>
-            <div>Item 3</div>
-            <div>Item 4</div>
-            <div>Item 5</div>
-            <div>Item 6</div>
+            <div className='item-carousel'>
+                <img src={imagesURL + item1?.image} />
+                <Link
+                    to={'/showitem/' + item1.product_id}>
+                    <p>
+                        {item1.name}
+                    </p>
+                </Link>
+                <p>{parseFloat(item1.price).toFixed(2)}€</p>
+                <button class="btn btn-primary" type="button"><TbIcons.TbShoppingCartPlus /> Add to cart</button>
+            </div>
+            <div className='item-carousel'>
+            <img src={imagesURL + item2?.image} />
+                <Link
+                    to={'/showitem/' + item2.product_id}>
+                    <p>
+                        {item2.name}
+                    </p>
+                </Link>
+                <p>{parseFloat(item2.price).toFixed(2)}€</p>
+                <button class="btn btn-primary" type="button"><TbIcons.TbShoppingCartPlus /> Add to cart</button>
+            </div>
+            <div className='item-carousel'>
+            <img src={imagesURL + item3?.image} />
+                <Link
+                    to={'/showitem/' + item3.product_id}>
+                    <p>
+                        {item3.name}
+                    </p>
+                </Link>
+                <p>{parseFloat(item3.price).toFixed(2)}€</p>
+                <button class="btn btn-primary" type="button"><TbIcons.TbShoppingCartPlus /> Add to cart</button>
+            </div>
+            <div className='item-carousel'>
+            <img src={imagesURL + item4?.image} />
+                <Link
+                    to={'/showitem/' + item4.product_id}>
+                    <p>
+                        {item4.name}
+                    </p>
+                </Link>
+                <p>{parseFloat(item4.price).toFixed(2)}€</p>
+                <button class="btn btn-primary" type="button"><TbIcons.TbShoppingCartPlus /> Add to cart</button>
+            </div>
+            <div className='item-carousel'>
+            <img src={imagesURL + item5?.image} />
+                <Link
+                    to={'/showitem/' + item5.product_id}>
+                    <p>
+                        {item5.name}
+                    </p>
+                </Link>
+                <p>{parseFloat(item5.price).toFixed(2)}€</p>
+                <button class="btn btn-primary" type="button"><TbIcons.TbShoppingCartPlus /> Add to cart</button>
+            </div>
+            <div className='item-carousel'>
+            <img src={imagesURL + item6?.image} />
+                <Link
+                    to={'/showitem/' + item6.product_id}>
+                    <p>
+                        {item6.name}
+                    </p>
+                </Link>
+                <p>{parseFloat(item6.price).toFixed(2)}€</p>
+                <button class="btn btn-primary" type="button"><TbIcons.TbShoppingCartPlus /> Add to cart</button>
+            </div>
         </Carousel>
     )
 }
