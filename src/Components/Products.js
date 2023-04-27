f162ae4c28da6ca698488e65cd74416fabfead2f
n2posa00 <n2posa00@students.oamk.fi>
2023-04-27 19:28:34 +0300

    Code that shows discount prices on category page

--- src/Components/Products.js
+++ src/Components/Products.js
@@ -3,7 +3,7 @@ import React, { useState, useEffect } from 'react';
 import { Link, useParams } from 'react-router-dom';
 import * as TbIcons from 'react-icons/tb';
 
-const imagesURL ='http://localhost:8000/images/';
+const imagesURL = 'http://localhost:8000/images/';
 
 export default function Products({ url }) {
     const [categoryName, setCategoryName] = useState('');
@@ -24,19 +24,19 @@ export default function Products({ url }) {
         axios.get(address)
             .then((response) => {
                 const json = response.data;
-                if(params.searchPhrase === undefined) {
+                if (params.searchPhrase === undefined) {
                     setCategoryName(json.category);
                     setProducts(json.products);
                 } else {
                     setCategoryName(params.searchPhrase);
                     setProducts(json);
                 }
-            // })
-        // axios.get(url + 'products/getproducts.php/' + params.categoryId)
-        //     .then((response) => {
-        //         const json = response.data;
-        //         setCategoryName(json.category);
-        //         setProducts(json.products);
+                // })
+                // axios.get(url + 'products/getproducts.php/' + params.categoryId)
+                //     .then((response) => {
+                //         const json = response.data;
+                //         setCategoryName(json.category);
+                //         setProducts(json.products);
             }).catch(error => {
                 alert(error.response === undefined ? error : error.response.data.error);
             })
@@ -48,14 +48,25 @@ export default function Products({ url }) {
             <h3 className='products-category'>Products for {categoryName}</h3>
             {products.map(product => (
                 <div key={product.product_id}>
-                    <img className='product-image'src={imagesURL + product.image} alt={product.name} />
+                    <img className='product-image' src={imagesURL + product.image} alt={product.name} />
                     <Link
                         to={'/showitem/' + product.product_id}>
                         <p>
                             {product.name}
                         </p>
                     </Link>
-                    <p className='product-price'>Price: {product.price.toFixed(2)} €</p>
+                    {product.lowered_price == null ? (
+                        <p className="product-price">Price: {product.price.toFixed(2) + "€"}</p>
+                    ) : (
+                        <div>
+                            <p className="original-price">
+                            Price:    {product.price.toFixed(2) + "€"}
+                            </p>
+                            <p className="lowered-price">
+                            Price:    {product.lowered_price.toFixed(2) + "€"}
+                            </p>
+                        </div>
+                    )}
                     <button className='btn product-btn btn-primary' type="button"><TbIcons.TbShoppingCartPlus />Add to cart</button>
                 </div>
             ))}
