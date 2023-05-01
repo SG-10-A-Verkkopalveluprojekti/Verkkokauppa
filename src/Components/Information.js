3f5ffbe591fc90ba1d62a1a76f9768a019c49ea6
JuusoPesonen <c2peju00@students.oamk.fi>
2023-05-01 19:53:49 +0300

    Created information.js page and elements for it

---
+++ src/Components/Information.js
@@ -0,0 +1,38 @@
+import React from "react";
+import InfoCard from './InfoCard';
+
+export default function Information() {
+    const cardData = [
+        {
+            title: 'Juuso Pesonen',
+            text: '- Done: - Products listing ideas and planning. - Logo and half of products images done with Midjourney AI. - Front page banner images. - Products by category section and products shown on each category. - Category buttons on front page. - Search bar functionality. - Feedback page. - Information page. -  Total of hours done: '
+        },
+        {
+            title: 'Samuli Pohjola',
+            text: 'Another example text to showcase the flexibility of the card component. - Total of hours done: '
+        },
+        {
+            title: 'Tuomas Kärki',
+            text: 'Yet another example text to demonstrate the reusability of the InfoCard component. - Total of hours done: '
+        },
+        {
+            title: 'Riku Ketomäki',
+            text: 'This is the final example text, but you can customize it as needed. - Total of hours done: ',
+        },
+    ];
+
+    const splitText = (text) => text.split('-').filter(item => item.trim() !== '');
+
+    return (
+        <div className="container information-container">
+            <h2 className="text-center text-white mb-4">Information about project site</h2>
+            <div className="row">
+                {cardData.map((data, index) => (
+                    <div key={index} className="col-md-6 col-lg-3 mb-4">
+                        <InfoCard title={data.title} textItems={splitText(data.text)} />
+                    </div>
+                ))}
+            </div>
+        </div>
+    );
+}
